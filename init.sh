bcd99997f5109308467789e9926d5ccf5a67e837
Max Meadowcroft <maxmeadowcroft61@gmail.com>
2024-04-18 12:20:02 -0600

    empty directory for refactoring

--- init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Only uncomment if needing to reinitialize the database. All local data will be lost.
-#flask --app cookr init-db
-python3 -m venv .venv
-. .venv/bin/activate
-flask --app cookr run --debug
-echo "Website: http://127.0.0.1:5000/"
