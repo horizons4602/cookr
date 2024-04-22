f72f5a4266ff3e0e14578777c119db27a8245138
Max Meadowcroft <maxmeadowcroft61@gmail.com>
2024-04-22 02:44:55 -0600

    added styles for Index

--- cookr/recipes/views.py
+++ cookr/recipes/views.py
@@ -4,112 +4,86 @@ from .models import Recipe
 from accounts.models import UserPreferences, UserAPIkeys, Profile
 import requests
 
-
-# IN THE FUTURE ADD CALORIES PARAMETER
 def edamam_api_call(user):
-    user_api_keys = UserAPIkeys.objects.get(user=user)
-    user_preferences = UserPreferences.objects.get(user=user)
-    user_profile = Profile.objects.get(user=user)
-
-    health_labels = []
-
-    for field in UserPreferences._meta.fields:
-        if field.name != "user":
-            if getattr(user_preferences, field.name):
+    try:
+        user_api_keys = UserAPIkeys.objects.get(user=user)
+        user_preferences = UserPreferences.objects.get(user=user)
+        user_profile = Profile.objects.get(user=user)
+        health_labels = []
+        for field in UserPreferences._meta.fields:
+            if field.name != "user" and getattr(user_preferences, field.name):
                 health_labels.append(field.name.replace("_", "-").lower())
 
-    if user_profile.goal == 0:
-        diet = "balanced"
-    elif user_profile.goal == 1:
-        diet = "low-carb"
-    else:
-        diet = "high-protein"
+        if user_profile.goal == 0:
+            diet = "balanced"
+        elif user_profile.goal == 1:
+            diet = "low-carb"
+        else:
+            diet = "high-protein"
 
-    url = "https://api.edamam.com/api/recipes/v2"
+        url = "https://api.edamam.com/api/recipes/v2"
+        params = {
+            "type": "public",
+            "app_id": user_api_keys.edamam_app_id,
+            "app_key": user_api_keys.edamam_api_key,
+            "diet": diet,
+            "health": health_labels,
+            "random": True,
+            "field": ["image", "label", "url", "ingredients", "calories", "totalTime", "totalNutrients"],
+        }
 
-    recipe_data = {
-        "Image_Url": [],
-        "Name": [],
-        "Time": [],
-        "Calories": [],
-        "Fat": [],
-        "Carbs": [],
-        "Protein": [],
-        "Ingredients": [],
-        "SiteUrl": [],
-        "Items": 0,
-        "Seen": 0,
-    }
+        response = requests.get(url, params=params)
+        if response.status_code != 200:
+            return "API_CALL_FAILED"
 
-    params = {
-        "type": "public",
-        "app_id": user_api_keys.edamam_app_id,
-        "app_key": user_api_keys.edamam_api_key,
-        "diet": diet,
-        "health": health_labels,
-        "random": True,
-        "field": ["image", "label", "url", "ingredients", "calories", "totalTime", "totalNutrients"],
-    }
-
-    response = requests.get(url,params=params)
-
-    if response.status_code == 200:
         data = response.json()
         recipes = data.get('hits', [])
-
-        if recipes:
-            for recipe in recipes:
-                recipe_info = recipe.get('recipe')
-                if recipe_info:
-                    name = recipe_info.get('label')
-                    image_url = recipe_info.get('image')
-                    site_url = recipe_info.get('url')
-                    calories = recipe_info.get('calories')
-                    time_to_cook = recipe_info.get('totalTime')
-                    fat = recipe_info.get('totalNutrients', {}).get('FAT', {}).get('quantity')
-                    carbs = recipe_info.get('totalNutrients', {}).get('CHOCDF', {}).get('quantity')
-                    protein = recipe_info.get('totalNutrients', {}).get('PROCNT', {}).get('quantity')
-                    ingredients_data = recipe_info.get('ingredients', [])
-                    ingredients = [ingredient.get('food') for ingredient in ingredients_data]
-
-                    recipe_data["Image_Url"].append(image_url)
-                    recipe_data["Name"].append(name)
-                    recipe_data["Time"].append(time_to_cook)
-                    recipe_data["Calories"].append(calories)
-                    recipe_data["Protein"].append(protein)
-                    recipe_data["Ingredients"].append(ingredients)
-                    recipe_data["SiteUrl"].append(site_url)
-                    recipe_data["Fat"].append(fat)
-                    recipe_data["Carbs"].append(carbs)
-                    recipe_data["Items"] += 1
-
-            return recipe_data
-        else:
-            print("NONE")
+        if not recipes:
             return "NONE FOUND"
+
+        recipe_data = {"Image_Url": [], "Name": [], "Time": [], "Calories": [], "Fat": [], "Carbs": [], "Protein": [],
+                       "Ingredients": [], "SiteUrl": [], "Items": 0, "Seen": 0}
+        for recipe in recipes:
+            recipe_info = recipe.get('recipe')
+            if recipe_info:
+                recipe_data["Image_Url"].append(recipe_info.get('image'))
+                recipe_data["Name"].append(recipe_info.get('label'))
+                recipe_data["Time"].append(recipe_info.get('totalTime'))
+                recipe_data["Calories"].append(recipe_info.get('calories'))
+                recipe_data["Protein"].append(recipe_info.get('totalNutrients', {}).get('PROCNT', {}).get('quantity'))
+                recipe_data["Ingredients"].append(
+                    [ingredient.get('food') for ingredient in recipe_info.get('ingredients', [])])
+                recipe_data["SiteUrl"].append(recipe_info.get('url'))
+                recipe_data["Fat"].append(recipe_info.get('totalNutrients', {}).get('FAT', {}).get('quantity'))
+                recipe_data["Carbs"].append(recipe_info.get('totalNutrients', {}).get('CHOCDF', {}).get('quantity'))
+                recipe_data["Items"] += 1
+
+        return recipe_data
+
+    except UserAPIkeys.DoesNotExist:
+        return "API_KEY_MISSING"  # Handle the case where API keys are missing
 
 
 class Main(TemplateView):
     template_name = "recipes/main.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
+    def get(self, request, *args, **kwargs):
+        user = request.user
         recipe_instance, created = Recipe.objects.get_or_create(user=user)
-
-        # Attempt to fetch an unseen recipe
         recipe = recipe_instance.get_next_recipe()
 
         if not recipe:
-            # If no unseen recipes are available, make an API call
-            recipe = edamam_api_call(user)
-            if recipe != "NONE FOUND":
-                recipe_instance.add_recipes(recipe)
-                recipe = recipe_instance.get_next_recipe()  # Retrieve the first of the newly added recipes
+            recipe_data = edamam_api_call(user)
+            if recipe_data in ["API_KEY_MISSING", "API_CALL_FAILED", "NONE FOUND"]:
+                # Return a different response if there's an issue with API keys or API call
+                return render(request, "recipes/error_template.html", {"error": recipe_data})
 
-        # Assign recipe details to the context if available
+            recipe_instance.add_recipes(recipe_data)
+            recipe = recipe_instance.get_next_recipe()
+
         if recipe:
-            context.update({
+            print(recipe["Image_Url"])
+            context = {
                 "image": recipe["Image_Url"],
                 "name": recipe["Name"],
                 "time": recipe["Time"],
@@ -119,6 +93,9 @@ class Main(TemplateView):
                 "fat": round(recipe["Fat"]),
                 "carbohydrates": round(recipe["Carbs"]),
                 "site": recipe["SiteUrl"],
-            })
+            }
+        else:
+            context = {}
 
-        return context
+        context = self.get_context_data(**context)
+        return self.render_to_response(context)
