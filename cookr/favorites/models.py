ed4936cca01fb905d2c43ca83e8d37825b06b4fa
Max Meadowcroft <maxmeadowcroft61@gmail.com>
2024-04-21 23:47:59 -0600

    added 6 concrete saved classes

--- cookr/favorites/models.py
+++ cookr/favorites/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=255)
+    image_url = models.URLField(max_length=500)
+    site_url = models.URLField(max_length=500)
+    calories = models.FloatField()
+    time_to_cook = models.IntegerField(help_text="Time in minutes")
+    fat = models.FloatField(help_text="Fat in grams")
+    carbs = models.FloatField(help_text="Carbohydrates in grams")
+    protein = models.FloatField(help_text="Protein in grams")
+    ingredients = models.JSONField()
+
+    class Meta:
+        abstract = True
+
+
+class SavedOne(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='italian_recipe')
+
+
+class SavedTwo(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='mexican_recipe')
+
+
+class SavedThree(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='indian_recipe')
+
+
+class SavedFour(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='french_recipe')
+
+
+class SavedFive(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='chinese_recipe')
+
+
+class SavedSix(Recipe):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='american_recipe')
